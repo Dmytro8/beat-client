4dec68fe2abed1fc5fef55c9b2d7b0658087e87e
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-05-06 22:41:28 +0200

    change local links to global

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,13 +2,15 @@ export const API_BASE_URL_LOCAL = "http://localhost:8080";
 export const API_BASE_URL_GLOBAL = "https://beatstart.herokuapp.com";
 export const ACCESS_TOKEN = "accessToken";
 
-export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
+export const OAUTH2_REDIRECT_LOCAL = "http://localhost:3000/oauth2/redirect";
+export const OAUTH2_REDIRECT_GLOBAL =
+  "https://beat-client.web.app/oauth2/redirect";
 
 export const GOOGLE_AUTH_URL =
   API_BASE_URL_GLOBAL +
   "/oauth2/authorize/google?redirect_uri=" +
-  OAUTH2_REDIRECT_URI;
+  OAUTH2_REDIRECT_GLOBAL;
 export const FACEBOOK_AUTH_URL =
   API_BASE_URL_GLOBAL +
   "/oauth2/authorize/facebook?redirect_uri=" +
-  OAUTH2_REDIRECT_URI;
+  OAUTH2_REDIRECT_GLOBAL;
