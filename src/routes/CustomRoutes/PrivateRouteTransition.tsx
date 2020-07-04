85684ad3719800b0fa0d425d736f82348eef4e6b
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-07-04 20:41:33 +0200

    added private route

---
+++ src/routes/CustomRoutes/PrivateRouteTransition.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { MountTransition } from "../../components/common/MountTransition";
+import { AuthContext } from "../../contexts/AuthContext/AuthContext";
+import { HOME } from "../../constants/route.urls";
+
+type Props = {
+  exact?: boolean;
+  path: string;
+};
+
+export const PrivateRouteTransition: FC<Props> = ({
+  children,
+  exact = false,
+  path,
+  ...rest
+}) => {
+  const [authState, authDispatch]: any = useContext(AuthContext);
+  return (
+    <>
+      {!authState.isAuthenticated ? (
+        <Redirect to={HOME} />
+      ) : (
+        <Route exact={exact} path="/admang/service-center" {...rest}>
+          <MountTransition>{children}</MountTransition>
+        </Route>
+      )}
+    </>
+  );
+};
