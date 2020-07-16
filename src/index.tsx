410e34f6608a58c9c2e0153f296441eb9790e8b6
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-07-16 14:35:53 +0200

    small fixes

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import { App } from "./components/App";
-import { AuthProvider } from "./contexts/AuthContext/AuthContext";
+// import { AuthProvider } from "./contexts/AuthContext/AuthProvider";
 import { ProfileProvider } from "./contexts/ProfileContext/ProfileContext";
+import { AuthProvider } from "./contexts/AuthContext/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
