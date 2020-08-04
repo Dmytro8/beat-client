7b6fffe77a20faf5c5a1d0b4474c465ee977b61e
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-08-04 22:30:40 +0200

    experiment with song album background

--- src/components/ImgAmbilight/ImgAmbilight.jsx
+++ src/components/ImgAmbilight/ImgAmbilight.jsx
@@ -1,14 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { PlayerContext } from "../../contexts/PlayerContext/PlayerContext";
 import { AUDIO_IMAGE_SERVER } from "../../constants";
 import classes from "./ImgAmbilight.module.scss";
+import classNames from "classnames/bind";
 
 const ImgAmbilight = () => {
   const [statePlayer, dispatchPlayer] = useContext(PlayerContext);
+  let cx = classNames.bind(classes);
+  let bgAmbilightStyle = cx({
+    bgAmbilight: true,
+    bgAmbilightActive: !statePlayer.isPaused,
+  });
+
+  useEffect(() => {
+    return () => {};
+  }, [statePlayer.isPlaying]);
   return (
     <div className={classes.imageAmbilight}>
-      <div className={classes.bgAmbilight}>
+      <div className={bgAmbilightStyle}>
         <LazyLoadImage
           alt={statePlayer.currentSong.name}
           effect="blur"
